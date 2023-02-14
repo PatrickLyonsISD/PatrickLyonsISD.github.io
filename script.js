c9901bcfd135bb3d5020a6987ac76973c0c51719
Patrick Lyons <43409140+PatrickLyonsISD@users.noreply.github.com>
2023-02-14 00:26:16 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -24,3 +24,14 @@ function showLoading() {
     document.getElementById("loading").style.visibility = "hidden";
     document.getElementById("footer").style.visibility = "visible";
   }
+
+  // Get a reference to the burger button and nav menu
+const burgerButton = document.querySelector('.burger-button');
+const navMenu = document.querySelector('.menu');
+
+// Add a click event listener to the burger button
+burgerButton.addEventListener('click', function() {
+  // Toggle the "show" and "open" classes on the nav menu
+  navMenu.classList.toggle('show');
+  navMenu.classList.toggle('open');
+});
